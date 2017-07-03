b62c88565c3723ece638aa0d9eff89d79ffaf5dd
Ian Nastajus <ian.nastajus@gmail.com>
2017-07-02 23:18:17 -0400

    refactor generalization & more specific functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var points = [];
+var gridPoints = [];
+var dataPoints = [];
+var dataPointsContainer = [];
 var context;
 
 const CANVAS_WIDTH = 600;
@@ -18,26 +20,44 @@ function Point (x, y) {
     };
 }
 
-function Rectangle(width, height, rotation) {
-
-}
-
 function init () {
 
-    initSamplePoints();
+    //initTrapezoidPoints();
+    //initRandomDataPoints();
+    initRandomDataPointsContainer();
 
-
-//     var color = "lightblue";
-//     initLinesBetweenPoints(points, color);
 }
 
-function initSamplePoints() {
-    points.push(new Point(1,1));
-    points.push(new Point(4,4));
-    points.push(new Point(8,8));
-    points.push(new Point(9,9));
-    console.log (points);
+function initTrapezoidPoints() {
+    dataPoints.push(new Point(3,1));
+    dataPoints.push(new Point(1,5));
+    dataPoints.push(new Point(8,8));
+    dataPoints.push(new Point(10,2));
 }
+
+function initRandomDataPoints() {
+    const minX = 1, maxX = 19;
+    const minY = 1, maxY = 9;
+
+    for (var i = 0; i < 10; i++) {
+        var randX = Math.floor(Math.random() * (maxX - minX)) + minX;
+        var randY = Math.floor(Math.random() * (maxY - minY)) + minY;
+        dataPoints.push(new Point (randX, randY));
+    }
+}
+
+function initRandomDataPointsContainer() {
+    dataPointsContainer.push(new Point(2,9));
+    dataPointsContainer.push(new Point(1,8));
+    dataPointsContainer.push(new Point(5,5));
+    dataPointsContainer.push(new Point(13,1));
+    dataPointsContainer.push(new Point(19,2));
+    dataPointsContainer.push(new Point(16,6));
+}
+
+// function initRandomDataPointsContained(){
+//
+// }
 
 function renderCanvas() {
     window.onload = function () {
@@ -47,15 +67,16 @@ function renderCanvas() {
         context = canvas.getContext("2d");
         //context.font = "20px Georgia";
 
-        initAndDrawGrid();
+        initGridPoints(gridPoints, "lightgray");
+        drawPoints(gridPoints, "lightgray");
 
-        drawLines();
-        drawPoints();
+        drawLinesBetweenPoints(dataPointsContainer);
+        drawPoints(dataPointsContainer, null, true);
 
     }
 }
 
-function drawLines(fillStyle) {
+function drawLinesBetweenPoints(points, fillStyle) {
     var pNext;
     for (var p = 0; p < points.length; p++) {
         pNext = (p + 1) % points.length;
@@ -71,11 +92,12 @@ function drawLine(pointBegin, pointEnd, fillStyle) {
     context.stroke();
 }
 
-function drawPoints(fillStyle) {
+function drawPoints(points, fillStyle, drawText) {
     for (var p = 0; p < points.length; p++) {
         drawPoint(points[p], fillStyle);
-        //console.log (points[p].toString());
-        drawPointText(points[p], fillStyle);
+        if (drawText) {
+            drawPointText(points[p], fillStyle);
+        }
     }
 }
 
@@ -93,8 +115,7 @@ function drawPointText(point, fillStyle) {
     context.fillText(point.toString(), point.canvasX + CANVAS_TEXT_OFFSET, point.canvasY + CANVAS_TEXT_OFFSET);
 }
 
-function initAndDrawGrid() {
-    var gridPoints = [];
+function initGridPoints(gridPoints) {
     const GRID_SQUARE_SIZE = 30;
 
     //this is not easy to read easily. refactor to be most readable possible:
@@ -102,10 +123,6 @@ function initAndDrawGrid() {
         for (var y = 0; y <= CANVAS_HEIGHT; y += GRID_SQUARE_SIZE ) {
             gridPoints.push(new Point(x / GRID_SQUARE_SIZE, y / GRID_SQUARE_SIZE));
         }
-    }
-
-    for (var g = 0; g < gridPoints.length; g++) {
-        drawPoint(gridPoints[g], "lightgray");
     }
 }
 
