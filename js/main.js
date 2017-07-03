5b964b20b410531d2644b712c233b114933e70d1
Ian Nastajus <ian.nastajus@gmail.com>
2017-07-02 23:53:46 -0400

    linear algebraic line sample added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-var gridPoints = [];
-var dataPoints = [];
-var dataPointsContainer = [];
 var context;
-
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 300;
 const CANVAS_SCALE = 30;
 const POINT_RADIUS = 5;
+
+var gridPoints = [];
+var dataPoints = [];
+var dataPointsContainer = [];
+var hypothesisLinePoints = [];
+var potentialCorrectLinePoints =[];
 
 function Point (x, y) {
     this.x = x;
     this.y = y;
 
     this.canvasX = x * CANVAS_SCALE;
-    this.canvasY = y * CANVAS_SCALE;
+    this.canvasY = CANVAS_HEIGHT - (y * CANVAS_SCALE);
 
     Point.prototype.toString = function () {
         return "(" + this.x + "," + this.y + ")";
@@ -24,7 +26,10 @@ function init () {
 
     //initTrapezoidPoints();
     //initRandomDataPoints();
-    initRandomDataPointsContainer();
+    //initRandomDataPointsContainer();
+    initSpreadPoints();
+    initLinearAlgebraLine(hypothesisLinePoints, 3, 1/2);
+    initLinearAlgebraLine(potentialCorrectLinePoints, -2, 1/3);
 
 }
 
@@ -59,6 +64,24 @@ function initRandomDataPointsContainer() {
 //
 // }
 
+function initSpreadPoints() {
+    dataPoints.push(new Point(1,1));
+    dataPoints.push(new Point(3,4));
+    dataPoints.push(new Point(2,5));
+    dataPoints.push(new Point(3,6));
+    dataPoints.push(new Point(5,5));
+    dataPoints.push(new Point(5,9));
+    dataPoints.push(new Point(6,4));
+    dataPoints.push(new Point(7,7));
+    dataPoints.push(new Point(7,8));
+    dataPoints.push(new Point(8,7));
+    dataPoints.push(new Point(9,9));
+    dataPoints.push(new Point(12,8));
+    dataPoints.push(new Point(13,9));
+    dataPoints.push(new Point(14,7));
+    dataPoints.push(new Point(18,8));
+}
+
 function renderCanvas() {
     window.onload = function () {
         var canvas = document.getElementById("canvas");
@@ -70,9 +93,11 @@ function renderCanvas() {
         initGridPoints(gridPoints, "lightgray");
         drawPoints(gridPoints, "lightgray");
 
-        drawLinesBetweenPoints(dataPointsContainer);
-        drawPoints(dataPointsContainer, null, true);
+        //drawLinesBetweenPoints(dataPoints);
+        drawPoints(dataPoints, "darkred", true);
 
+        drawLinesBetweenPoints(hypothesisLinePoints, "darkred");
+        drawLinesBetweenPoints(potentialCorrectLinePoints, "black");
     }
 }
 
@@ -84,11 +109,11 @@ function drawLinesBetweenPoints(points, fillStyle) {
     }
 }
 
-function drawLine(pointBegin, pointEnd, fillStyle) {
+function drawLine(pointBegin, pointEnd, strokeStyle) {
     context.beginPath();
     context.moveTo(pointBegin.canvasX, pointBegin.canvasY);
     context.lineTo(pointEnd.canvasX, pointEnd.canvasY);
-    context.fillStyle = (fillStyle === undefined) ? "#000000" : fillStyle;
+    context.strokeStyle = (strokeStyle === null) ? context.strokeStyle : strokeStyle;
     context.stroke();
 }
 
@@ -105,13 +130,13 @@ function drawPoint(point, fillStyle) {
     context.beginPath();
     context.arc(point.canvasX, point.canvasY, POINT_RADIUS, 0, Math.PI*2, true);
     context.closePath();
-    context.fillStyle = (fillStyle === undefined) ? "#000000" : fillStyle;
+    context.fillStyle = (fillStyle === null) ? context.fillStyle : fillStyle;
     context.fill();
 }
 
 function drawPointText(point, fillStyle) {
     const CANVAS_TEXT_OFFSET = 10;
-    context.fillStyle = (fillStyle === undefined) ? "#000000" : fillStyle;
+    context.fillStyle = (fillStyle === null) ? context.fillStyle : fillStyle;
     context.fillText(point.toString(), point.canvasX + CANVAS_TEXT_OFFSET, point.canvasY + CANVAS_TEXT_OFFSET);
 }
 
@@ -124,6 +149,23 @@ function initGridPoints(gridPoints) {
             gridPoints.push(new Point(x / GRID_SQUARE_SIZE, y / GRID_SQUARE_SIZE));
         }
     }
+}
+
+function initLinearAlgebraLine(linePoints, b0, b1) {
+    //y=mx + b
+    //y_hat = b0 + b1 * x;
+    //y_hat = y_intercept + slope * (x);
+    // y = 3 + 1/2 * x;
+
+    var y_axis_interecept = new Point(0, b0);
+    var slope = b1; //unused
+
+    var x_max = CANVAS_WIDTH / CANVAS_SCALE;
+    var canvas_right_side_intercept = new Point(x_max, b0 + b1 * x_max);
+
+    linePoints.push(y_axis_interecept);
+    linePoints.push(canvas_right_side_intercept);
+
 }
 
 init();
