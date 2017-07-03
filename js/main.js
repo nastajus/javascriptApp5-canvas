852284c06ea855ebc0203886c9f84c86c6908e06
Ian Nastajus <ian.nastajus@gmail.com>
2017-07-03 00:09:48 -0400

    cleanup + refactor to new Line protoype

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const POINT_RADIUS = 5;
 
 var gridPoints = [];
 var dataPoints = [];
-var dataPointsContainer = [];
 var hypothesisLinePoints = [];
 var potentialCorrectLinePoints =[];
 
@@ -24,45 +23,15 @@ function Point (x, y) {
 
 function init () {
 
-    //initTrapezoidPoints();
-    //initRandomDataPoints();
-    //initRandomDataPointsContainer();
     initSpreadPoints();
-    initLinearAlgebraLine(hypothesisLinePoints, 3, 1/2);
-    initLinearAlgebraLine(potentialCorrectLinePoints, -2, 1/3);
+
+    //initLinearAlgebraLine(hypothesisLinePoints, 3, 1/2);
+    hypothesisLinePoints = new Line(3, 1/2).endPoints();
+
+    //initLinearAlgebraLine(potentialCorrectLinePoints, -2, 1/3);
+    potentialCorrectLinePoints = new Line(-2, 1/3).endPoints();
 
 }
-
-function initTrapezoidPoints() {
-    dataPoints.push(new Point(3,1));
-    dataPoints.push(new Point(1,5));
-    dataPoints.push(new Point(8,8));
-    dataPoints.push(new Point(10,2));
-}
-
-function initRandomDataPoints() {
-    const minX = 1, maxX = 19;
-    const minY = 1, maxY = 9;
-
-    for (var i = 0; i < 10; i++) {
-        var randX = Math.floor(Math.random() * (maxX - minX)) + minX;
-        var randY = Math.floor(Math.random() * (maxY - minY)) + minY;
-        dataPoints.push(new Point (randX, randY));
-    }
-}
-
-function initRandomDataPointsContainer() {
-    dataPointsContainer.push(new Point(2,9));
-    dataPointsContainer.push(new Point(1,8));
-    dataPointsContainer.push(new Point(5,5));
-    dataPointsContainer.push(new Point(13,1));
-    dataPointsContainer.push(new Point(19,2));
-    dataPointsContainer.push(new Point(16,6));
-}
-
-// function initRandomDataPointsContained(){
-//
-// }
 
 function initSpreadPoints() {
     dataPoints.push(new Point(1,1));
@@ -88,12 +57,10 @@ function renderCanvas() {
         canvas.width = CANVAS_WIDTH;
         canvas.height = CANVAS_HEIGHT;
         context = canvas.getContext("2d");
-        //context.font = "20px Georgia";
 
         initGridPoints(gridPoints, "lightgray");
         drawPoints(gridPoints, "lightgray");
 
-        //drawLinesBetweenPoints(dataPoints);
         drawPoints(dataPoints, "darkred", true);
 
         drawLinesBetweenPoints(hypothesisLinePoints, "darkred");
@@ -151,21 +118,24 @@ function initGridPoints(gridPoints) {
     }
 }
 
-function initLinearAlgebraLine(linePoints, b0, b1) {
+function Line(b0, b1) {
+
+    this.y_intercept_y_value = b0;
+    this.slope = b1;
+
+    Line.prototype.endPoints = function() {
+        const x_max = CANVAS_WIDTH / CANVAS_SCALE;
+        var y_at_x_max = this.y_intercept_y_value + this.slope * x_max;
+
+        var p1 = new Point (0, this.y_intercept_y_value); //y-intercept
+        var p2 = new Point (x_max, y_at_x_max);
+        return [p1, p2];
+    };
+
     //y=mx + b
     //y_hat = b0 + b1 * x;
     //y_hat = y_intercept + slope * (x);
     // y = 3 + 1/2 * x;
-
-    var y_axis_interecept = new Point(0, b0);
-    var slope = b1; //unused
-
-    var x_max = CANVAS_WIDTH / CANVAS_SCALE;
-    var canvas_right_side_intercept = new Point(x_max, b0 + b1 * x_max);
-
-    linePoints.push(y_axis_interecept);
-    linePoints.push(canvas_right_side_intercept);
-
 }
 
 init();
